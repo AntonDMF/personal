24478ae5cf603bc5c01cf180c47b717a1957d9d3
AntonDMF <antoniodm7@gmail.com>
2026-05-06 16:49:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
+const SUPABASE_URL = "https://xonkfaybccwruiavqxvr.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvbmtmYXliY2N3cnVpYXZxeHZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwOTg2OTEsImV4cCI6MjA5MzY3NDY5MX0.I980bCbIBCBXeUUbLwJKyLKU_0sxRdCDwCl8Gw37Gew";
+
+const EVENT_SLUG = "main-event";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
